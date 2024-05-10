6e0dd6a9c4f9bb7636793519b20fb2fefdd68acf
Hamdoune Chaymae <89420843+Hchaymae@users.noreply.github.com>
2024-05-10 09:43:03 +0100

    Adding Patient Form and List

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    # We want it to automatically populate it whenever we want to make a new instance of this note
-    created_at = models.DateTimeField(auto_now_add=True)
-    # on to many relationship
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+class Patient(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    STATUS_CHOICES = [
+        ('NC', 'Not Consulted'),
+        ('C', 'Consulting'),
+        ('CO', 'Consulted'),
+    ]
+
+
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    age = models.PositiveIntegerField()
+
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default='NC',
+    )
 
     def __str__(self):
-        return self.title 
+        return f"{self.firstname} {self.lastname}"
