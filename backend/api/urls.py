ca7aa34db8748ca5492eb2db61ac1bf0982f3a8c
Hamdoune Chaymae <89420843+Hchaymae@users.noreply.github.com>
2024-05-10 09:45:12 +0100

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # this link is for creating and listing
-    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    # pk stands for primary key
-    path("notes/delete<int:pk>/", views.NoteDelete.as_view(), name="delete-note")
+    path('add_patient/', views.CreatePatientView.as_view(), name='add_patient'),
+    path('patients/', views.patient_list, name='patients'),
 ]
